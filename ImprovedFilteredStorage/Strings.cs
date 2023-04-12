8c25751b6c365f15582cdb1c8c4719146080d8f5
Luboš Luňák <l.lunak@centrum.cz>
2023-04-12 21:57:22 +0200

    use a shorter button label to prevent it from being 2 lines

--- ImprovedFilteredStorage/Strings.cs
+++ ImprovedFilteredStorage/Strings.cs
@@ -8,8 +8,8 @@ namespace ImprovedFilteredStorage
 {
     public static class Strings
     {
-        public static LocString BUTTON_ENABLE = "Enable Partitioned Storage";
-        public static LocString BUTTON_DISABLE = "Disable Partitioned Storage";
+        public static LocString BUTTON_ENABLE = "Enable Partitions";
+        public static LocString BUTTON_DISABLE = "Disable Partitions";
 
         public static LocString NOCONTENT = "Nothing selected";
     }
